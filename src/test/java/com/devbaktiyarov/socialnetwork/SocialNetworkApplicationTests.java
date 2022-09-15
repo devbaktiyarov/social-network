bda5aa339ea34782b85c52414033e73475f49c96
Arsen Baktiyarov <devbaktiyarov@gmail.com>
2022-09-15 17:50:37 +0600

    Initial commit for Social network api

---
+++ src/test/java/com/devbaktiyarov/socialnetwork/SocialNetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devbaktiyarov.socialnetwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialNetworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+	
+}
